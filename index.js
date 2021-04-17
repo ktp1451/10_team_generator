f65d352687d98511125d62b178873b3952a5df1c
ktp1451 <76793752+ktp1451@users.noreply.github.com>
2021-04-17 17:40:20 -0500

    udpated index

--- index.js
+++ index.js
@@ -1,1 +1,115 @@
+const inquirer = require("inquirer")
+const fs = require("fs")
 const Manager = require("./lib/manager")
+const generateTemplate = require("./src/template")
+
+let team = []
+
+function init (){
+    function manager(){
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "",
+            },
+            {
+                type: "input",
+                name: "office",
+                message: "",
+            }
+        ]).then(function(answers){
+            let manager = new Manager(answers.name,answers.email,answers.id,answers.office)
+            team.push(manager)
+            fs.writeFileSync("dist/output.html", generateTemplate(team))
+            inquirer.prompt([
+                {
+                    message: "do you want to add an engineer?"
+                }
+            ]).then(function(data){
+                if(data.answer == "engineer") {
+                    engineer()
+                }
+            })
+        })
+    }
+    
+    function engineer(){
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "",
+            },
+            {
+                type: "input",
+                name: "github",
+                message: "",
+            }
+        ]).then(function(answers){
+            let engineer = new Engineer(answers.name,answers.email,answers.id,answers.github)
+            team.push(engineer)
+            fs.writeFileSync("dist/output.html", generateTemplate(team))
+            inquirer.prompt([
+                {
+                    message: "do you want to add an intern?"
+                }
+            ]).then(function(data){
+                if(data.answer == "intern") {
+                    intern()
+                }
+            })
+        })
+    }
+
+    function intern(){
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "name",
+                message: "",
+            },
+            {
+                type: "input",
+                name: "email",
+                message: "",
+            },
+            {
+                type: "input",
+                name: "id",
+                message: "",
+            },
+            {
+                type: "input",
+                name: "school",
+                message: "",
+            }
+        ]).then(function(answers){
+            let engineer = new Intern(answers.name,answers.email,answers.id,answers.school)
+            team.push(intern)
+            fs.writeFileSync("dist/output.html", generateTemplate(team))
+        })
+    }
+}
+
+init();
---
+++ src/template.js
@@ -0,0 +1,12 @@
+function generateTemplate(team){
+    return `
+    <HTML>
+    <head>
+    <head>
+    <body>
+        ${team[0].name}
+    <body>
+    `
+}
+
+module.exports = generateTemplate
